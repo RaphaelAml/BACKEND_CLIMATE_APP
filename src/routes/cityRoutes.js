849380056b3da451d1feffaee9de96ddc0fd485b
RaphaelAml <raphaelaml1@outlook.com>
2024-10-25 22:00:16 -0300

    att

--- src/routes/cityRoutes.js
+++ src/routes/cityRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const cityControl = require("../controllers/cityController");
+const cityController = require("../controllers/wheatherForecastController");
 
-router.post("/weather", cityControl.addCityAndGetWeather); //contexto é de wheather / forecast, mover esse endpoint / metodos agregados para as devidas classes
+router.post("/weather", cityController.addCityAndGetWeather); //contexto é de wheather / forecast, mover esse endpoint / metodos agregados para as devidas classes
 
 router.get("/city", cityControl.getCityByName);
 
